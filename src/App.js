21d36960537d787302fb01bc98764b90b903a68b
Liam Morgan <lite@getignited.co.uk>
2017-06-16 07:52:21 +0100

    Updated to v0.2.0

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import Isvg                 from 'react-inlinesvg'
 
 import logo                 from './xivcurrents.svg'
 import logoFallback         from './xivcurrents-light.png'
-import './App.css'
 
-import Grid                 from './Components/Grid/Grid.jsx'
-import Footer               from './Components/Footer/Footer.jsx'
+import Grid                 from './Components/Grid'
+import Footer               from './Components/Footer'
 
 class App extends Component {
   render() {
@@ -26,9 +25,19 @@ class App extends Component {
           </div>
         </header>
 
-        <main className="container">
-          <Grid />
+        <main className="pageContainer">
+          <div className="siteContent">
+            <div className="container">
+              <p>XIVCurrents is a tracker for Aether Currents in Final Fantasy XIV. If you want to get flying (and swimming, in Stormblood!) sooner, then this is the site for you.</p>
+              <p>As of Stormblood launching, a lot of data will be in flux over the course of early access. Heavensward data also needs confirming, as data has changed since I made my original list 2 years back!</p>
+              <p>With that said, I hope this site is of use to you! Any issues you have, please <a className="link" href="">open a new request</a> on the GitHub page.</p>
+              <p>Enjoy Stormblood, folks!</p>
+            </div>
+
+            <Grid />
+          </div>
         </main>
+
         <Footer />
       </div>
     )
