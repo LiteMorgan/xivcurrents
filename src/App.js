3689cc4cece7b9113285af318f6bdfc538d84d14
Liam Morgan <lite@getignited.co.uk>
2017-06-17 04:25:10 +0100

    Added update messages

--- src/App.js
+++ src/App.js
@@ -28,6 +28,9 @@ class App extends Component {
         <main className="pageContainer">
           <div className="siteContent">
             <div className="container">
+              <p><strong>Update, 17/6/17:</strong> Another number value fluff, my apologies! Current 4 in The Peaks may be marked off already, and Quest 4 (Saint Slayer (Lv. 61)) may not be. Please keep this in mind! Sorry, it's 3am here. :D.</p>
+            </div>
+            <div className="container">
               <p>XIVCurrents is a tracker for Aether Currents in Final Fantasy XIV. If you want to get flying (and swimming, in Stormblood!) sooner, then this is the site for you.</p>
               <p>As of Stormblood launching, a lot of data will be in flux over the course of early access. Heavensward data also needs confirming, as data has changed since I made my original list 2 years back!</p>
               <p>With that said, I hope this site is of use to you! Any issues you have, please <a className="link" href="">open a new request</a> on the GitHub page.</p>
