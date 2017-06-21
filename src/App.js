88c5096324b574849b5be3e179e4637692ebdf3c
Liam Morgan <lite@getignited.co.uk>
2017-06-21 07:38:37 +0100

    Updated notices

--- src/App.js
+++ src/App.js
@@ -28,12 +28,12 @@ class App extends Component {
         <main className="pageContainer">
           <div className="siteContent">
             <div className="container">
-              <p><strong>Update, 17/6/17:</strong> Another number value fluff, my apologies! Current 4 in The Peaks may be marked off already, and Quest 4 (Saint Slayer (Lv. 61)) may not be. Please keep this in mind! Sorry, it's 3am here. :D.</p>
+              <p><strong>Update, 19/6/17:</strong> It is done! All the Currents are found, so go earn those wings! Any feedback, comments or suggestions, please feel free to open an issue on the Github, or drop a comment on the <a className="link" href="https://www.reddit.com/r/ffxiv/comments/6hpmbc/xivcurrents_aether_current_tracker_now_updating/" target="_blank" rel="noreferrer noopener">Reddit post</a>. Would be much appreciated! Thanks for taking part, and enjoy Stormblood!</p>
             </div>
             <div className="container">
               <p>XIVCurrents is a tracker for Aether Currents in Final Fantasy XIV. If you want to get flying (and swimming, in Stormblood!) sooner, then this is the site for you.</p>
               <p>As of Stormblood launching, a lot of data will be in flux over the course of early access. Heavensward data also needs confirming, as data has changed since I made my original list 2 years back!</p>
-              <p>With that said, I hope this site is of use to you! Any issues you have, please <a className="link" href="">open a new request</a> on the GitHub page.</p>
+              <p>With that said, I hope this site is of use to you! Any issues you have, please <a className="link" href="https://github.com/getignited/xivcurrents" target="_blank" rel="noreferrer noopener">open a new request</a> on the GitHub page.</p>
               <p>Enjoy Stormblood, folks!</p>
             </div>
 
